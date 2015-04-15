d807ad0a9fea4ed135eea1c4088679770000afd1
rgodinez <cyrik13@yahoo.com>
2015-04-15 02:11:28 -0400

    Work baby

--- linkstate.java
+++ linkstate.java
@@ -1,36 +1,56 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.ArrayList;
+
 
 public class linkstate{
 
-	public static void main(String[] args){
-	
-		Scanner scan = new Scanner(System.in);
+	public static int[][] fileRead(String fileName) throws IOException
+	{
+		ArrayList<String> text = new ArrayList<String>();
 		
-		int[][] matrix = new int[6][6];
-		int[] distance = new int[6];
-		int[] visited = new int[6];
-		int[] preD = new int[6];
+		BufferedReader in = new BufferedReader( new FileReader(fileName));
+		String temp = in.readLine();
+		
+		while(temp.compareTo("EOF.") != 0)
+		{
+			text.add(temp);
+			temp = in.readLine();
+		}
+		in.close();
+		
+		int max = text.size();
+		int[][] map = new int[max][max];
+		
+		for(int i = 0; i < map.length; i++)
+		{
+			for(int j = 0; j < map.length; j++)
+			{
+				if(text.get(i).charAt(j*2) == 'N')
+					map[i][j] = 999;
+				else
+					map[i][j] = Integer.parseInt(text.get(i).charAt(j*2) + "");
+			}
+		}
+		
+		return map;
+	}
+	
+	public static void main(String[] args) throws IOException{
+		
+		int[][] matrix = fileRead("network.txt"); // TODO: make sure it's args[0]
+		
+		int[] distance = new int[matrix.length];
+		int[] visited = new int[matrix.length];
+		int[] preD = new int[matrix.length];
 		int min;
 		int nextNode = 0;
 		
-		System.out.println("Enter the Matrix!");
+		System.out.println("--------------------------------------------------------------------------");
+		System.out.print("Step\tN\'");
 		
-		for(int i = 0; i < 6; i++){
-		
-			visited[i] = 0;
-			
-			preD[i] = 0;
-			
-			for(int j = 0; j < 6; j++){
-			
-				matrix[i][j] = scan.nextInt();
-				
-				if(matrix[i][j] == 99)
-					matrix[i][j] = 999;
-			
-			}
-		
-		}
+		for(int i = 1; i < matrix.length; i++)
+			System.out.print("\tD(" + i + "),p(" + i + ")");
+		System.out.println("--------------------------------------------------------------------------");
 		
 		distance = matrix[0];
 		distance[0] = 0;
@@ -38,69 +58,49 @@ public class linkstate{
 		
 		//algorithm starts here
 		
-		for(int i = 0; i < 6; i++){
-			
+		for(int i = 0; i < 6; i++)
+		{	
 			min = 999;
 			
 			for(int j = 0; j < 6; j++){
-				
 				if(min > distance[j] && visited[j]!=1){
 					
 					min = distance[j];
 					nextNode = j;
-					
 				}
-				
 			}
 			
 			visited[nextNode] = 1;
 			
 			// now we start the actual algorithm
-			
 			for(int c =0; c < 6; c++){
-				
 				if(visited[c]!=1){
-					
 					if(min+matrix[nextNode][c]<distance[c]){
 						
 						distance[c]=min+matrix[nextNode][c];
-						preD[c] = nextNode;
-						
-					}
-					
-				}
-				
+						preD[c] = nextNode;	
+					}	
+				}	
 			}
-			
 		}
 		
-		for(int i = 0; i < 6; i++){
-			
+		for(int i = 0; i < 6; i++)
 			System.out.print("|" + distance[i]);
-			
-			
-		}
+		
 		System.out.print("|");
 		
 		//for printing paths
-		
 		for(int i = 0; i < 6; i++){
-			
-			int j;
-			System.out.println("Path = " + i);
-			
-			j=i;
+			int j = i;
+			System.out.println("\nPath = " + i);
 			
 			do{
 				
 				j = preD[j];
 				System.out.print(" <- " + j); 
 			}while(j != 0);
-			
-			System.out.println();
-			
-			
 		}
+		
 	}
 
 }
